7b99ecb861f3c937fdfc8e4fdac7d4dbd84d447d
feliperezende86 <52765682+feliperezende86@users.noreply.github.com>
2019-10-12 21:36:25 -0300

    Create Sqoop - CTR.scala
    Sqoop de carga inicial com todos os registros existentes na tabela

---
+++ Sqoop - CTR.scala
@@ -0,0 +1,10 @@
+sqoop import -Dhadoop.security.credential.provider.path=jceks://hdfs/user/bigrco04/db2.db2mis01.jceks \
+--connect jdbc:db2://bdb2p04.plexbsb.bb.com.br:50446/BDB2P04 \
+--username db2mis01 -password-alias db2.db2mis01.alias \
+--query "SELECT CHAR(DIGITS(T1.NR_UNCO_CTR_OPR) || DIGITS(T2.NR_EPRD_FNCD) || DIGITS(T2.NR_SCTR_OPR)) AS ROW_ID, \
+T1.NR_UNCO_CTR_OPR, T2.NR_EPRD_FNCD, T2.NR_SCTR_OPR, T1.SG_SIS_RSP_CDU, T1.CD_PRF_DEPE_CDU, T1.NR_CTR_OPR, T1.CD_EST_CDU, T1.DT_EST_CDU, T1.CD_CNL_FRMZ_CTR, T1.CD_PRF_DEPE_NEGR, \
+T1.CD_PRF_DEPE_EXEC, T1.CD_PRD, T1.CD_MDLD, T1.CD_IEC_MOE, T1.CD_CIA_RSP_CTR \
+FROM DB2OPR.CTR_OPR T1, DB2OPR.SCTR_OPR T2 \
+WHERE (t1.NR_UNCO_CTR_OPR = t2.NR_UNCO_CTR_OPR) and \$CONDITIONS" \
+--hbase-table 'dirco_gecig:rst506' --column-family 'ctr_opr' --hbase-row-key ROW_ID \
+--split-by t1.NR_UNCO_CTR_OPR -m 128
